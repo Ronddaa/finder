03b61c062f401e620fb007b3d0ea72b0ed350bc2
Ronddaa <tronddaa@gmail.com>
2026-01-11 21:51:14 +0100

    update fonts start

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage";
 import Footer from "./components/Footer/Footer";
+import useIsDesktop from "./hooks/useIsDesktop";
 
 export default function App() {
-
+const isDesktop = useIsDesktop(1024)
+  const marginTopForMain = isDesktop ? 120 : 60;
   return (
     <>
       <Header />
-      <main style={{ marginTop: "120px" }}>
+      <main style={{ marginTop:  marginTopForMain}}>
         <Routes>
           <Route path="/" element={<MainPage />} />
           
